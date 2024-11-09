337a1a236461c0dbbeb3a88a7ef75238a98aea6a
arydningen <arydningen@gmail.com>
2024-11-10 00:26:14 +0100

    Add help panel

--- src/ssh_browse.py
+++ src/ssh_browse.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import atexit
 import curses
+import curses.panel
 import ssh_hosts
 import tmux_split
 import pwd
@@ -34,6 +35,25 @@ def get_hosts_to_display(ssh_config_data, selected_category):
             hosts.append(k)
     return hosts
 
+def get_help_text():
+    title = "SSH Browse Help"
+    content = [
+        "<enter> - Connect to the selected host",
+        "<space> - Select/Deselect a host",
+        "Up/Down - Navigate through the hosts",
+        "Left/Right - Change the category",
+        "1-9 - Select a category by number",
+        "h - Toggle help",
+        "p - Ping all hosts to check reachability",
+        "t - Open selected hosts in tmux",
+        "e - View/Edit notes for the selected host",
+        "d - Run demo or die",
+        "q - Quit the application"
+    ]
+    choices = [" "]
+    selected_choice = 0
+    return title, content, choices, selected_choice
+
 def render_header(stdscr, col1_length, col2_length, spacer, COL_HEADER):
     stdscr.addstr(0, 4, 'Hosts', COL_HEADER)
     stdscr.addstr(0, col1_length, 'Properties', COL_HEADER)
@@ -52,17 +72,16 @@ def render_hosts(stdscr, hosts, ssh_config_data, selected_hosts, current_option,
             color = COL_UNKNOWN
         if host in selected_hosts:
             color = COL_SELECTION if color == COL_ACTIVE else COL_SELECTION | curses.A_DIM
-
         stdscr.addstr(i + top_margin, 4, pretext + host, color)
         if current_option == i:
             stdscr.addstr(i + top_margin, 1, '->', COL_ARROW)
 
-def render_properties(stdscr, ssh_config_data, hosts, current_option, top_margin, col1_length, COL_PROPERTIES, COL_ACTIVE, COL_INACTIVE):
+def render_properties(stdscr, ssh_config_data, hosts, current_option, top_margin, col1_length, COL_PROPERTIES,COL_UNKNOWN, COL_ACTIVE, COL_INACTIVE):
     hostname = hosts[current_option]
     selected_host_config = ssh_config_data[hostname]
     propertylist = list(selected_host_config.keys())
     valuelist = list(selected_host_config.values())
-    hostcolor = COL_ACTIVE if selected_host_config.get('Reachable') == 'yes' else COL_INACTIVE
+    hostcolor = COL_ACTIVE if selected_host_config.get('Reachable') == 'yes' else COL_INACTIVE if selected_host_config.get('Reachable') == 'no' else COL_UNKNOWN
     stdscr.addstr(0 + top_margin, col1_length, hostname, hostcolor)
     for i, (prop, val) in enumerate(zip(propertylist, valuelist)):
         stdscr.addstr(i + 1 + top_margin, col1_length, f'{prop}: {val}', COL_PROPERTIES)
@@ -77,21 +96,24 @@ def render_footer(stdscr, size, COL_FOOTER):
     stdscr.addstr(size.lines - 2, 1, "<space> - select  | t - tmux selected   | d - demo or die", COL_FOOTER)
     stdscr.addstr(size.lines - 1, 1, "<enter> - connect | e - view/edit notes | q - quit", COL_FOOTER)
 
-def render_centered_window(stdscr, title, content, choices, selected_choice, COL_WINDOW, COL_TITLE, COL_CONTENT, COL_CHOICES, COL_SELECTED_CHOICE, win=None):
+def render_centered_panel(stdscr, title, content, choices, selected_choice, COL_WINDOW, COL_TITLE, COL_CONTENT, COL_CHOICES, COL_SELECTED_CHOICE, panel=None):
     size = stdscr.getmaxyx()
     win_height = len(content) + len(choices) + 4
     win_width = max(len(title), max(len(line) for line in content), max(len(choice) for choice in choices)) + 4
     win_y = (size[0] - win_height) // 2
     win_x = (size[1] - win_width) // 2
-    win_x = 3
+    win_x = 15
+    win_y = 2
 
-    if win is None:
+    if panel is None:
         win = curses.newwin(win_height, win_width, win_y, win_x)
         win.bkgd(' ', COL_WINDOW)
         win.box()
-
-    win.clear()
-    win.box()
+        panel = curses.panel.new_panel(win)
+    else:
+        win = panel.window()
+        win.erase()
+        win.box()
 
     win.addstr(1, (win_width - len(title)) // 2, title, COL_TITLE)
 
@@ -102,9 +124,9 @@ def render_centered_window(stdscr, title, content, choices, selected_choice, COL
         color = COL_SELECTED_CHOICE if i == selected_choice else COL_CHOICES
         win.addstr(3 + len(content) + i, 2, choice, color)
 
-    # stdscr.refresh()
-    # win.refresh()
-    return win
+    #curses.panel.update_panels()
+    #stdscr.refresh()
+    return panel
 
 def init_colors():
     fgcols = []
@@ -134,6 +156,7 @@ class Theme:
             fgcols[name] = curses.color_pair(index)
         return fgcols
 
+# MARK: main
 def main(stdscr):
     # Load configuration from .ssh-browse file
     config_file_path = get_config_location()
@@ -143,12 +166,7 @@ def main(stdscr):
 
     # Extract settings from the configuration
     ping_on_startup = config.get('ping_on_startup', 'default_value')
-    
-    #theme = Theme('original_theme')
     theme = Theme(config.get('theme', 'plain_theme'))
-
-
-    help_window = None
 
     fgcols = theme.init_colors()
     COL_ACTIVE = fgcols['COL_ACTIVE']
@@ -167,6 +185,9 @@ def main(stdscr):
 
     stdscr.clear()
     command = ''
+
+    help_panel = None
+    help_panel_visible = False
 
     # Uses wsl2 compatible path as default
     ssh_config_location = config.get('ssh_config_location', get_ssh_config_location())
@@ -197,15 +218,22 @@ def main(stdscr):
         scroll_pos = (current_option % max_lines) + 1 if current_option >= max_lines else 0
 
         render_hosts(stdscr, hosts[scroll_pos:], ssh_config_data, selected_hosts, current_option, top_margin, COL_ACTIVE, COL_INACTIVE, COL_UNKNOWN, COL_SELECTION, COL_ARROW)
-        render_properties(stdscr, ssh_config_data, hosts, current_option, top_margin, col1_length, COL_PROPERTIES, COL_ACTIVE, COL_INACTIVE)
+        render_properties(stdscr, ssh_config_data, hosts, current_option, top_margin, col1_length, COL_PROPERTIES, COL_UNKNOWN, COL_ACTIVE, COL_INACTIVE)
         render_categories(stdscr, ssh_config_data, hosts, current_option, categories, selected_category, top_margin, col1_length, col2_length, spacer, COL_SELECTED_CATEGORY, COL_CATOGORY)
         render_footer(stdscr, size, COL_FOOTER)
-        # help_window = render_centered_window(stdscr, 'SSH Browse', ['Select a host to connect to', 'Press q to quit', ''], ['good','evil'], 0, COL_ACTIVE, COL_HEADER, COL_ACTIVE, COL_ACTIVE, COL_ACTIVE, help_window)
         
+        if help_panel_visible:
+            title, content, choices, selected_choice = get_help_text()
+            help_panel = render_centered_panel(stdscr, title, content, choices, selected_choice, COL_ACTIVE, COL_HEADER, COL_ACTIVE, COL_ACTIVE, COL_ACTIVE, help_panel)
+        else:
+            if help_panel:
+                help_panel.hide()
+                help_panel = None
+        curses.panel.update_panels()
+
+        # Refresh the screen
         stdscr.move(0, 0)
         stdscr.refresh()
-        if help_window:
-            help_window.refresh()
 
         stdscr.timeout(1000)
         action = stdscr.getch()
@@ -245,7 +273,9 @@ def main(stdscr):
         elif action == ord('d'):
             tmux_split.demo()
         elif action == ord('p'):
-            ssh_hosts.check_reachable_all(ssh_config_data, False) 
+            ssh_hosts.check_reachable_all(ssh_config_data, False)
+        elif action == ord('h'):
+            help_panel_visible = not help_panel_visible 
         elif action == ord('q'):
             break
 
